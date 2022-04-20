ffd4e62002bbc845b43ff8babcd307aa4eee0db2
Guvy2002 <100072843+Guvy2002@users.noreply.github.com>
2022-04-20 03:09:08 +0100

    Create KevinCV.php

---
+++ KevinCV.php
@@ -0,0 +1,189 @@
+<?php
+session_start();
+$_SESSION;
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel = "stylesheet" href="css/style.css" />  
+        <title> Zara Bond CV Web Page </title>
+        <meta charset="UTF-8">s
+    </head>
+    <body>
+    <!-- HEADER FOR PAGE -->
+        <h1> Aston CV Website </h1>
+        <!-- NAVIGATION BAR -->
+<style>
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #333;
+    }
+    
+    li {
+      float: center;
+      border-right:1px solid #bbb;
+    }
+    
+    li:last-child {
+      border-right: none;
+    }
+    
+    li a {
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+    }
+    
+    li a:hover:not(.active) {
+      background-color: #111;
+    }
+    
+    .active {
+      background-color: #04AA6D;
+    }
+    body {
+    background-color: white;
+    p.solid {border-style: solid;}
+  }
+  
+  h1 {
+    color: black;
+    text-align: center;
+    border: 5px solid black;
+  
+  }
+
+  h2 {
+    color: black;
+    text-align: center;
+    border: 5px solid black
+  
+  }
+
+  h3 {
+    color: black;
+    text-align: center;
+    border: 5px solid black
+  
+  }
+
+  h5 {
+    color: black;
+    text-align: center;
+    text-decoration: underline;
+  
+  }
+  
+  p {
+    font-family: verdana;
+    font-size: 20px;
+    text-align: center;
+  }
+  
+  a{
+    text-align: center;
+  
+  }
+  
+  br{
+    text-align: center;
+  
+  }
+  
+  
+  dl {
+    text-align: center;
+  
+  
+  }
+  
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 40%;
+  }
+  
+  ul{
+    text-align: center;
+  
+  }
+  
+    </style>
+    </head>
+    <body>
+    
+    <ul>
+      <li><a href="AstonCV.php">Home</a></li>
+      <li><a href="Login.php">Log in</a></li>
+      <li><a href="Register.php">Register</a></li>
+      <li><a href="PublicCV.php">CV's</a></li>
+    </ul>
+</body>
+</html>
+
+    
+    <body> 
+    
+<!-- CONTACT DETAILS -->
+
+      <h2>Contact Detail:</h2>
+<dl>
+  <dt>Email: Kevin.Smith@gmail.com</dt>
+  <dt>Contact Number: 0791234587</dt>
+</dl>
+
+<!-- EDUCATION -->
+<dl>
+<h2>Personal Summary:</h2>
+      <dd> • Detail-orientated Accountant wit 12 years effectivley maintaining accurate accouting information for large-scale financial organisations </dd>
+</dl>
+<dl>
+<h3> <dt>University:</dt></h3>
+  <dd> •  Bachelor of Science, Accounting & Finance 2010 Manchester Metropolitan University - Manchester   </dd>
+  <h3>  <dt>A-Levels:</dt> </h3>
+  <dd> • Mathematics, Economics and IT, 2007 </dd>
+  <dd> • Loreto College - Manchester </dd>
+
+<!-- WORK EXPERIANCE -->
+
+      <h2>Work Experiance:</h4>
+<dl>
+  <h5> Financial Accountant, 02/2019 - Current Michael Philips Finance - Manchester </h5>
+  <dd> • Ensured effective working capital management and do further improve cash flow managment</dd>
+  <dd> • Determined problem areas and resolved issues related to profitability and business objective achievement</dd>
+  <dd> • Efficiently solved complex problems that impacted managment.</dd>
+</dl>
+
+<dl>
+    <h5> Junior Financial Accountant, 03/2014 to 02/2019 - Paige Accounting and Finance - Manchester </h5>
+    <dd> • Assisted with payroll function backup.	</dd>
+    <dd> • Enhanced audit controls by improving planning and testing processes.</dd>
+    <dd> • Followed up with customers to collect information and verify details.	</dd>
+</dl>
+
+<dl>
+    <h5> Trainee Acountant 11/2010 to 03/2014 - Santander UK Manchester</h5>
+    <dd> • Completed financial reports to inform managers and stakeholders.</dd>
+    <dd> • Approved daily cash entries and inputted returned cheeks entries into the accounting system.	</dd>
+    <dd> • Analysed departmental documents for appropriate distrbution and filing.</dd>
+</dl>
+
+<!-- SKILLS -->
+
+       <h2>Skills:</h4>
+<dl>
+    <dd> 	Booking </dd>
+    <dd> 	Budgeting  </dd>
+    <dd>    String verbal communication  </dd>
+    <dd> 	Microsoft Office </dd>
+    <dd> 	Attension to detail. </dd>
+    <dd> 	Customer care. </dd>
+    <dd> 	IT skills. </dd>
+    <dd> 	Data management. </dd>
+</dl>
