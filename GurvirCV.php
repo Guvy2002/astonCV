2c5f288cff445495b49c6d0634f087fd80d4fa1f
Guvy2002 <100072843+Guvy2002@users.noreply.github.com>
2022-04-20 03:10:52 +0100

    Create GurvirCV.php

---
+++ GurvirCV.php
@@ -0,0 +1,214 @@
+<?php
+session_start();
+$_SESSION;
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel = "stylesheet" href="css/style.css" />  
+        <title> Gurvir Brar CV Web Page </title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <!-- HEADER FOR PAGE -->
+        <h1> Aston CV Website </h1>
+        <!-- NAVIGATION BAR -->
+<style>
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #333;
+    }
+    
+    li {
+      float: center;
+      border-right:1px solid #bbb;
+    }
+    
+    li:last-child {
+      border-right: none;
+    }
+    
+    li a {
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+    }
+    
+    li a:hover:not(.active) {
+      background-color: #111;
+    }
+    
+    .active {
+      background-color: #04AA6D;
+    }
+    body {
+    background-color: white;
+    p.solid {border-style: solid;}
+  }
+  
+  h1 {
+    color: black;
+    text-align: center;
+    border: 5px solid black;
+  
+  }
+
+  h2 {
+    color: black;
+    text-align: center;
+    border: 5px solid black
+  
+  }
+
+  h3 {
+    color: black;
+    text-align: center;
+    border: 5px solid black
+  
+  }
+
+  h5 {
+    color: black;
+    text-align: center;
+    text-decoration: underline;
+  
+  }
+  
+  p {
+    font-family: verdana;
+    font-size: 20px;
+    text-align: center;
+  }
+  
+  a{
+    text-align: center;
+  
+  }
+  
+  br{
+    text-align: center;
+  
+  }
+  
+  
+  dl {
+    text-align: center;
+  
+  
+  }
+  
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 40%;
+  }
+  
+  ul{
+    text-align: center;
+  
+  }
+  
+    
+    </style>
+    </head>
+    <body>
+    
+    <ul>
+      <li><a href="AstonCV.php">Home</a></li>
+      <li><a href="Login.php">Log in</a></li>
+      <li><a href="Register.php">Register</a></li>
+      <li><a href="PublicCV.php">CV's</a></li>
+    </ul>
+</body>
+</html>
+
+    
+    <body> 
+    
+<!-- CONTACT DETAILS -->
+
+      <h2>Contact Detail:</h2>
+<dl>
+  <dt>Email: gurvirbrar36@gmail.com</dt>
+  <dt>Contact Number: 07474427949</dt>
+</dl>
+
+<!-- EDUCATION -->
+
+      <h2>Education:</h2>
+<dl>
+<h3> <dt>University:</dt></h3>
+  <dd> •  September 2021 COMPUTER SCIENCE WITH BUSINESS UNDERGRADUATE AT ASTON UNIVERSITY.  </dd>
+  <h3>  <dt>A-levels 2019-2021:</dt> </h3>
+  <dd> • 2 A-LEVLS IN BUSINESS AND SOCIOLOGY, GRADE A IN BOTH</dd>
+  <dd> •  D* C-TEC QUALIFICATION IN CAMBRIDGE TECHNICALS IN I.T </dd>
+  <h3>  <dt> GCSE'S 2017-2019:</dt></h3>
+  <dd> • Mathematics - 7</dd>
+  <dd> • Science - 6,6</dd>
+  <dd> • English literature - 7</dd>
+  <dd> • English language - 6</dd>
+  <dd> • R.E - 7</dd>
+  <dd> • History - 6</dd>
+  <dd> • Computer Science - 5</dd>
+  <dd> • Economics - 6</dd>
+  <dd> • Spanish - 5</dd>
+</dl>
+
+<!-- WORK EXPERIANCE -->
+
+      <h2>Work Experiance:</h4>
+<dl>
+  <h5> MAY – AUGUST 2021 RETAIL STORE COLLEAGUE, SLOUGH BAGGAGE OUTLET:</h5>
+  <dd> •	Supported and carefully listened to all customers queries and questions. </dd>
+  <dd> •	Worked confidently as part of a team and individually when carrying out tasks.  </dd>
+  <dd> •    Developed positive customer relationships through friendly greetings and excellent  service.  </dd>
+  <dd> •	Replenished products on shelves and displays for sales and product promotions. </dd>
+  <dd> •	Kept store clean and organised at all times to ensure optimal presentation fur customers at all times. </dd>
+  <dd> •	Fulfilled all retail duties in a high end sales outlet. Handled customer service including POS, sales and inventory management. </dd>
+</dl>
+
+<dl>
+    <h5> JUNE – AUGUST 2020 DATA ANALYTICS VIRTUAL INTERSHIP, KPMG:</h5>
+    <dd> •	I successfully scrutinised data, producing data insights, and presenting this in an accessible format for a wide range of stakeholders.  </dd>
+    <dd> •	I utilised various filtering techniques to remove low quality data, such as blank cells, from the dataset. </dd>
+    <dd> • 	This internship helped me identify how to utilise my problem-solving skills in a commercial context and built my confidence in communicating with clients and senior management. </dd>
+</dl>
+
+<!-- SKILLS -->
+
+       <h2>Skills:</h4>
+<dl>
+    <dd> 	Leadership. </dd>
+    <dd> 	Communication.  </dd>
+    <dd>    Managment Skills.  </dd>
+    <dd> 	Able to multi task. </dd>
+    <dd> 	Well organised. </dd>
+    <dd> 	Proactive. </dd>
+    <dl>
+      <dt> Three Programming Languages I Have Used:</dt>
+      <dd> • CSS - Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen, on paper, in speech, or on other media.	</dd>
+     <P> <img src="CSS.png" alt="CSS" width="250" height="200" </P>
+      <dd> • Java - Java is used as the server-side language for most back-end development projects, including those involving big data and Android development. Java is also commonly used for desktop computing, other mobile computing, games, and numerical computing. </dd>
+     <P> <img src="Java.png" alt="Java" width="250" height="200" </P>
+      <dd> • HTML	- HTML is the most basic building block of the Web. It defines the meaning and structure of web content.</dd>
+     </P>  <img src="HTML.png" alt="HTML" width="250" height="200"</P> 
+  </dl>
+
+</dl>
+
+<!-- HOBBIES & ACTIVITIES -->
+       <h2>Hobbies:</h4>
+    <p>I have made contributions to society through my passion for music production and dance. I am a member of a Mental Health Organisation which supports individuals experiencing mental health difficulties. Additionally, I have had the opportunity to captain 16 people for a highly competitive national dance competition in front of 1,500 people at the Sadler’s Wells Theatre, which ultimately developed my leadership, communication and project management skills.</p>
+
+<!-- LINKS TO PROFESSIONAL PROFILES-->
+       <h2>Links to professional profiles:</h4>
+    <p><a href="https://www.linkedin.com/in/gurvir-brar-018b211b2/">Visit My linkedin Profile!</a></p>
+    <p><a href="https://github.com/Guvy2002">Visit My Github Profile!</a></p>
+    </body>
+</html>
