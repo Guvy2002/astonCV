b9ed094586d5fef76eb393a7d2ec51057f13e4be
Guvy2002 <100072843+Guvy2002@users.noreply.github.com>
2022-04-20 03:05:07 +0100

    Create astoncv.php

---
+++ astoncv.php
@@ -0,0 +1,139 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel = "stylesheet" href="css/style.css" />  
+        <title> AstonCV Home Page</title>
+        <meta charset="UTF-8";>
+    </head>
+    <body>
+
+<!-- HEADER FOR PAGE -->
+
+        <h1> Aston CV Website </h1>
+
+<!-- NAVIGATION BAR -->
+<style>
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #333;
+    }
+    
+    li {
+      float: center;
+      border-right:1px solid #bbb;
+    }
+    
+    li:last-child {
+      border-right: none;
+    }
+    
+    li a {
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+    }
+    
+    li a:hover:not(.active) {
+      background-color: #111;
+    }
+    
+    .active {
+      background-color: #04AA6D;
+    }
+
+    <style>
+
+    body {
+    background-color: white;
+    p.solid {border-style: solid;}
+  }
+  
+  h1 {
+    color: black;
+    text-align: center;
+    border: 5px solid black;
+  }
+  
+  h2 {
+    color: black;
+    text-align: center;
+    border: 5px solid black
+  }
+
+  h3 {
+    color: black;
+    text-align: center;
+    border: 5px solid black
+  }
+
+  h5 {
+    color: black;
+    text-align: center;
+    text-decoration: underline;
+  }
+  
+  p {
+    font-family: verdana;
+    font-size: 20px;
+    text-align: center;
+  }
+  
+  a{
+    text-align: center;
+  }
+  
+  br{
+    text-align: center;
+  }
+  
+  
+  dl {
+    text-align: center;
+  }
+  
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 40%;
+  }
+  
+  ul{
+    text-align: center;
+  }
+
+  
+  </style>
+</style>
+    </head>
+    <body>
+    
+    <ul>
+      <li><a href="AstonCV.php">Home</a></li>
+      <li><a href="Login.php">Log in</a></li>
+      <li><a href="Register.php">Register</a></li>
+      <li><a href="PublicCV.php">CV's</a></li>
+    </ul>
+
+    <h2>Search Our CV Database</h2>
+    <dl>
+        <dd>Take control of your hiring and find the ideal person quickly by using our CV Database to search over 2.9 million profiles. You can browse unlimited CVs, with the option to get further information and contact up to 100 people within a month.</dd>
+
+    </dl>
+    <P> <img src="CV Desktop.jpg" alt="CV" width="450" height="200" </P>
+    
+    <dl>
+        <h3> What you get</h3>
+        <h5> Unlimited searching  </h5>
+        <dd> Make as many CV searches as you need and view summaries of relevant candidates</dd>   
+        <h5> Find local candidates	 </h5>
+        <dd> Sort your candidates by the time it would take them to commute to your workplace </dd>
+    </dl>  
+  </body>
